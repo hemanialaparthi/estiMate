97daf96cd4dda820f10fafe6d3f410cb1f2adad3
hemanialaparthi <alaparthi01@allegheny.edu>
2026-04-07 00:26:16 -0400

    feat: changes to deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'http://localhost:4000',
+                target: 'https://estimate-yjne.onrender.com',
                 changeOrigin: true,
             },
         },
